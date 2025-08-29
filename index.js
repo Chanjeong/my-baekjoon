c1f08c0e470977fee4b7b16d1f8f4742200dfec3
chanjeong park <ckswjd9595@gmail.com>
2025-08-29 20:44:19 +0900

    백준 1015번 - 수열 정렬

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 let input = require('fs')
-  .readFileSync('C:/Users/ckswj/Desktop/Zerobase/Backjoon/example.txt', 'utf-8')
+  .readFileSync('C:/Users/ckswj/Desktop/SUDURUK/backjoon/example.txt', 'utf-8')
   .toString()
   .trim()
   .split('\n');
 // let input = require("fs").readFileSync("/dev/stdin").toString().trim().split('\n');
 
-const N = +input[0];
+const a = +input[0];
+const b = input[1].split(' ').map(Number);
 
-let a = 1,
-  b = 0;
+const sorted = [...b].sort((a, b) => a - b);
+const visited = new Array(a).fill(false);
+const P = new Array(a);
 
-for (let i = 0; i < N; i++) {
-  let temp = a;
-  a = b;
-  b = temp + b;
+for (let i = 0; i < a; i++) {
+  const target = b[i];
+
+  for (let j = 0; j < a; j++) {
+    if (sorted[j] === target && !visited[j]) {
+      P[i] = j;
+      visited[j] = true;
+      break;
+    }
+  }
 }
-console.log(a, b);
+console.log(P.join(' '));
