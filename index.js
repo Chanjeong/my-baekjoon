ed54e2ecd7f916cf066a714f43c6b6f0c8d8be98
chanjeong park <ckswjd9595@gmail.com>
2025-08-30 15:42:48 +0900

    백준 10817번

--- index.js
+++ index.js
@@ -5,15 +5,6 @@ let input = require('fs')
   .split('\n');
 // let input = require("fs").readFileSync("/dev/stdin").toString().trim().split('\n');
 
-const N = +input[0];
-const P = input[1].split(' ').map(Number);
+const N = input[0].split(' ').map(Number);
 
-const sorted = [...P].sort((a, b) => a - b);
-
-const min = [];
-const sum = sorted.reduce((acc, cum) => {
-  min.push(acc + cum);
-  return acc + cum;
-}, 0);
-
-console.log(min.reduce((acc, cum) => acc + cum, 0));
+console.log(N.sort((a, b) => a - b)[1]);
