e9244712465d7d2ca9ec7aa355225daf0ee6117f
chanjeong park <ckswjd9595@gmail.com>
2025-08-30 15:39:19 +0900

    백준 11399번

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
 let input = require('fs')
-  .readFileSync('C:/Users/ckswj/Desktop/SUDURUK/backjoon/example.txt', 'utf-8')
+  .readFileSync('C:/Users/ckswj/Desktop/MYLIFE/backjoon/example.txt', 'utf-8')
   .toString()
   .trim()
   .split('\n');
 // let input = require("fs").readFileSync("/dev/stdin").toString().trim().split('\n');
 
-const a = +input[0];
-const b = input[1].split(' ').map(Number);
+const N = +input[0];
+const P = input[1].split(' ').map(Number);
 
-const sorted = [...b].sort((a, b) => a - b);
-const visited = new Array(a).fill(false);
-const P = new Array(a);
+const sorted = [...P].sort((a, b) => a - b);
 
-for (let i = 0; i < a; i++) {
-  const target = b[i];
+const min = [];
+const sum = sorted.reduce((acc, cum) => {
+  min.push(acc + cum);
+  return acc + cum;
+}, 0);
 
-  for (let j = 0; j < a; j++) {
-    if (sorted[j] === target && !visited[j]) {
-      P[i] = j;
-      visited[j] = true;
-      break;
-    }
-  }
-}
-console.log(P.join(' '));
+console.log(min.reduce((acc, cum) => acc + cum, 0));
