658146b6d7c377530276d81891087ebeb78e3f55
chanjeong park <ckswjd9595@gmail.com>
2025-08-30 16:18:25 +0900

    백준 -1920번 수 찾기 @

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ let input = require('fs')
   .split('\n');
 // let input = require("fs").readFileSync("/dev/stdin").toString().trim().split('\n');
 
-const N = input[0].split(' ').map(Number);
+const sortedA = input[1]
+  .split(' ')
+  .map(Number)
+  .sort((a, b) => a - b);
+const B = input[3].split(' ').map(Number);
 
-console.log(N.sort((a, b) => a - b)[1]);
+function binarySearch(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (arr[mid] === target) return 1;
+    else if (arr[mid] < target) left = mid + 1;
+    else right = mid - 1;
+  }
+  return 0;
+}
+console.log(B.map(target => binarySearch(sortedA, target)).join('\n'));
