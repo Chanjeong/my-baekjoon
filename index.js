08f3cfa6a8aaf2ad19483f54f01ff30b8c388965
chanjeong park <ckswjd9595@gmail.com>
2025-09-02 14:32:25 +0900

    1026번 보물

--- index.js
+++ index.js
@@ -5,5 +5,15 @@ let input = require('fs')
   .split('\n');
 // let input = require("fs").readFileSync("/dev/stdin").toString().trim().split('\n');
 
-const N = input[0].split(' ').map(Number);
-console.log(N.sort((a, b) => a - b).join(' '));
+const N = +input[0];
+const A = input[1]
+  .split(' ')
+  .map(Number)
+  .sort((a, b) => a - b);
+const B = input[2]
+  .split(' ')
+  .map(Number)
+  .sort((a, b) => b - a);
+const result = new Array(N).fill(0);
+const shit = result.map((_, i) => A[i] * B[i]);
+console.log(shit.reduce((a, b) => a + b));
