95b69c721a1c419e6d5ce1fedb7265d7f25759f8
chanjeong park <ckswjd9595@gmail.com>
2025-09-02 14:20:32 +0900

    백준 2752번 세수정렬

--- index.js
+++ index.js
@@ -4,19 +4,6 @@ let input = require('fs')
   .trim()
   .split('\n');
 // let input = require("fs").readFileSync("/dev/stdin").toString().trim().split('\n');
-const meetings = input
-  .slice(1)
-  .map(line => line.split(' ').map(Number))
-  .sort((a, b) => a[1] - b[1] || a[0] - b[0]);
 
-let count = 0;
-let lastEnd = 0;
-
-for (const [s, e] of meetings) {
-  if (s >= lastEnd) {
-    count++;
-    lastEnd = e;
-  }
-}
-
-console.log(count);
+const N = input[0].split(' ').map(Number);
+console.log(N.sort((a, b) => a - b).join(' '));
