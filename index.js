afd8666029cb888f60e9646e4d60af6000bd116b
chanjeong park <ckswjd9595@gmail.com>
2025-09-04 15:12:46 +0900

    1946 - 신입 사원 @

--- index.js
+++ index.js
@@ -5,26 +5,24 @@ let input = require('fs')
   .split('\n');
 // let input = require("fs").readFileSync("/dev/stdin").toString().trim().split('\n');
 
-const N = +input[0];
-const arr = input[1]
-  .split(' ')
-  .map(Number)
-  .sort((a, b) => a - b);
-const x = +input[2];
+let cases = input.slice(1).map(item => item.split(' ').map(Number));
 
-let left = 0;
-let right = arr.length - 1;
-let count = 0;
-while (left < right) {
-  let sum = arr[left] + arr[right];
-  if (sum === x) {
-    count++;
-    left++;
-    right--;
-  } else if (sum < x) {
-    left++;
-  } else {
-    right--;
+for (let i = 0; i < cases.length; i++) {
+  if (cases[i].length === 1) {
+    let n = cases[i][0];
+    let arr = cases.slice(i + 1, i + 1 + n);
+    arr.sort(([a, b], [c, d]) => a - c);
+
+    let count = 1; // 첫 번째 지원자는 무조건 선발
+    let minInterview = arr[0][1]; // 첫 번째 지원자의 면접 등수
+
+    for (let j = 1; j < arr.length; j++) {
+      if (arr[j][1] < minInterview) {
+        count++;
+        minInterview = arr[j][1]; // 최소 면접 등수 업데이트
+      }
+    }
+    console.log(count);
+    i += n; // 다음 테스트 케이스로 이동
   }
 }
-console.log(count);
