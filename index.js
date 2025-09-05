343b5f5200dd9256ca26d1d0a86421c0fe6dfeae
chanjeong park <ckswjd9595@gmail.com>
2025-09-05 13:58:11 +0900

    11656 접미사 배열

--- index.js
+++ index.js
@@ -5,24 +5,9 @@ let input = require('fs')
   .split('\n');
 // let input = require("fs").readFileSync("/dev/stdin").toString().trim().split('\n');
 
-let cases = input.slice(1).map(item => item.split(' ').map(Number));
-
-for (let i = 0; i < cases.length; i++) {
-  if (cases[i].length === 1) {
-    let n = cases[i][0];
-    let arr = cases.slice(i + 1, i + 1 + n);
-    arr.sort(([a, b], [c, d]) => a - c);
-
-    let count = 1; // 첫 번째 지원자는 무조건 선발
-    let minInterview = arr[0][1]; // 첫 번째 지원자의 면접 등수
-
-    for (let j = 1; j < arr.length; j++) {
-      if (arr[j][1] < minInterview) {
-        count++;
-        minInterview = arr[j][1]; // 최소 면접 등수 업데이트
-      }
-    }
-    console.log(count);
-    i += n; // 다음 테스트 케이스로 이동
-  }
+const n = input[0];
+const arr = [];
+for (let i = 0; i < n.length; i++) {
+  arr.push(n.slice(n.length - 1 - i, n.length));
 }
+console.log(arr.sort().join('\n'));
