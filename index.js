7e8438f9838bd432a5068cb9c9ec4712d0305a85
chanjeong park <ckswjd9595@gmail.com>
2025-09-16 15:19:10 +0900

    DP - 2156 - 포도주 서식 @

--- index.js
+++ index.js
@@ -6,11 +6,18 @@ let input = require('fs')
 // let input = require("fs").readFileSync("/dev/stdin").toString().trim().split('\n');
 
 const n = Number(input[0]);
+const wine = input.slice(1).map(Number);
 const dp = new Array(n + 1).fill(0);
 
-dp[0] = 0;
-dp[1] = 1;
-for (let i = 2; i <= n; i++) {
-  dp[i] = BigInt(dp[i - 2]) + BigInt(dp[i - 1]);
+dp[1] = wine[0];
+dp[2] = wine[0] + wine[1];
+dp[3] = Math.max(wine[0] + wine[1], wine[0] + wine[2], wine[1] + wine[2]);
+
+for (let i = 4; i <= n; i++) {
+  dp[i] = Math.max(
+    dp[i - 3] + wine[i - 2] + wine[i - 1],
+    dp[i - 2] + wine[i - 1],
+    dp[i - 1]
+  );
 }
-console.log(dp[n].toString());
+console.log(dp[n]);
