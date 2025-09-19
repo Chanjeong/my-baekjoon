20917ff25c9440d2326071bcaf49567d86e42aa3
chanjeong park <ckswjd9595@gmail.com>
2025-09-19 14:53:23 +0900

    DP - 2747 - 피보나치 수

--- index.js
+++ index.js
@@ -7,15 +7,13 @@ let input = require('fs')
 
 const n = Number(input[0]);
 
-// 이친수는 피보나치 수열과 같습니다
-// dp[i] = i자리 이친수의 개수
 const dp = new Array(n + 1).fill(0);
 
-dp[1] = 1; // 1자리: 1
-dp[2] = 1; // 2자리: 10
+dp[1] = 1;
+dp[2] = 1;
 
 for (let i = 3; i <= n; i++) {
-  dp[i] = BigInt(dp[i - 1]) + BigInt(dp[i - 2]);
+  dp[i] = dp[i - 1] + dp[i - 2];
 }
 
-console.log(dp[n].toString());
+console.log(dp[n]);
