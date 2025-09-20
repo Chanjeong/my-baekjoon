29afb510052f971f6d2324a1cfd9992d6266c00e
chanjeong park <ckswjd9595@gmail.com>
2025-09-20 15:19:48 +0900

    DP - 11660 - 구간 합 구하기5 @

--- index.js
+++ index.js
@@ -5,21 +5,23 @@ let input = require('fs')
   .split('\n');
 // let input = require("fs").readFileSync("/dev/stdin").toString().trim().split('\n');
 
-const str1 = input[0].trim();
-const str2 = input[1].trim();
-
-const dp = Array.from({ length: str1.length + 1 }, () =>
-  Array(str2.length + 1).fill(0)
-);
-
-for (let i = 1; i <= str1.length; i++) {
-  for (let j = 1; j <= str2.length; j++) {
-    if (str1[i - 1] === str2[j - 1]) {
-      dp[i][j] = dp[i - 1][j - 1] + 1;
-    } else {
-      dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-    }
-  }
+const [N, M] = input[0].split(' ').map(Number);
+const arr = [];
+for (let i = 1; i <= N; i++) {
+  arr.push(input[i].split(' ').map(Number));
 }
 
-console.log(dp[str1.length][str2.length]);
+const dp = Array.from({ length: N + 1 }, () => Array(N + 1).fill(0));
+
+for (let i = 1; i <= N; i++) {
+  for (let j = 1; j <= N; j++) {
+    dp[i][j] =
+      dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + arr[i - 1][j - 1];
+  }
+}
+for (let i = N + 1; i <= N + M; i++) {
+  const [x1, y1, x2, y2] = input[i].split(' ').map(Number);
+  const result =
+    dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1];
+  console.log(result);
+}
