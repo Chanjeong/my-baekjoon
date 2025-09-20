f9cfaa3c7c7889b99e6fd6de43407f8b385534a4
chanjeong park <ckswjd9595@gmail.com>
2025-09-20 15:56:55 +0900

    DP - 1904 - 01타일

--- index.js
+++ index.js
@@ -5,23 +5,13 @@ let input = require('fs')
   .split('\n');
 // let input = require("fs").readFileSync("/dev/stdin").toString().trim().split('\n');
 
-const [N, M] = input[0].split(' ').map(Number);
-const arr = [];
-for (let i = 1; i <= N; i++) {
-  arr.push(input[i].split(' ').map(Number));
-}
+const n = Number(input[0]);
+const dp = new Array(n + 1).fill(0);
 
-const dp = Array.from({ length: N + 1 }, () => Array(N + 1).fill(0));
+dp[1] = 1;
+dp[2] = 2;
 
-for (let i = 1; i <= N; i++) {
-  for (let j = 1; j <= N; j++) {
-    dp[i][j] =
-      dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + arr[i - 1][j - 1];
-  }
+for (let i = 3; i <= n; i++) {
+  dp[i] = dp[i - 2] + (dp[i - 1] % 15746);
 }
-for (let i = N + 1; i <= N + M; i++) {
-  const [x1, y1, x2, y2] = input[i].split(' ').map(Number);
-  const result =
-    dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1];
-  console.log(result);
-}
+console.log(dp[n] % 15746);
