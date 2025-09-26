45a8b1820254110cb0051dc1cce646e52e3d798c
chanjeong park <ckswjd9595@gmail.com>
2025-09-26 15:22:14 +0900

    DP - 1699 - 제곱수의 합 @

--- index.js
+++ index.js
@@ -5,18 +5,14 @@ let input = require('fs')
   .split('\n');
 // let input = require("fs").readFileSync("/dev/stdin").toString().trim().split('\n');
 
-const n = Number(input[0]);
-const arr = input[1].split(' ').map(Number);
+let n = Number(input[0]);
 
-const dp = new Array(n).fill(0);
-dp[0] = arr[0];
+let dp = new Array(n + 1).fill(Infinity);
+dp[0] = 0;
 
-for (let i = 1; i < n; i++) {
-  dp[i] = arr[i];
-  for (let j = 0; j < i; j++) {
-    if (arr[j] < arr[i]) {
-      dp[i] = Math.max(dp[i], dp[j] + arr[i]);
-    }
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j * j <= i; j++) {
+    dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
   }
 }
-console.log(Math.max(...dp));
+console.log(dp);
