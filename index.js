3f00cf2dd671108652a1594ce8d558704eeb51a3
chanjeong park <ckswjd9595@gmail.com>
2025-10-07 14:58:59 +0900

    DP - 11048 - 이동하기

--- index.js
+++ index.js
@@ -5,20 +5,17 @@ let input = require('fs')
   .split('\n');
 // let input = require("fs").readFileSync("/dev/stdin").toString().trim().split('\n');
 
-const n = +input[0];
-const MOD = 9901;
+const [n, m] = input[0].split(' ').map(Number);
 
-const dp = Array.from({ length: n }, () => new Array(3).fill(0));
+const dp = Array.from({ length: n + 1 }, () => new Array(m + 1).fill(0));
 
-for (let i = 0; i <= 2; i++) {
-  dp[0][i] = 1;
+const maze = input.slice(1).map(e => e.split(' ').map(Number));
+
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= m; j++) {
+    dp[i][j] =
+      Math.max(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) +
+      maze[i - 1][j - 1];
+  }
 }
-
-for (let i = 1; i < n; i++) {
-  dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % MOD;
-  dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % MOD;
-  dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % MOD;
-}
-
-const sum = (dp[n - 1][0] + dp[n - 1][1] + dp[n - 1][2]) % MOD;
-console.log(sum);
+console.log(dp[n][m]);
