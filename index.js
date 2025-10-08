a8819c5973d955aa9c49aec611d713b071a50cbf
chanjeong park <ckswjd9595@gmail.com>
2025-10-08 14:19:32 +0900

    DP - 24416 - 알고리즘 수업 - 피보나치 수 1

--- index.js
+++ index.js
@@ -5,17 +5,27 @@ let input = require('fs')
   .split('\n');
 // let input = require("fs").readFileSync("/dev/stdin").toString().trim().split('\n');
 
-const [n, m] = input[0].split(' ').map(Number);
+const n = +input[0];
+let fibCount = 0;
+let fibonacciCount = 0;
 
-const dp = Array.from({ length: n + 1 }, () => new Array(m + 1).fill(0));
-
-const maze = input.slice(1).map(e => e.split(' ').map(Number));
-
-for (let i = 1; i <= n; i++) {
-  for (let j = 1; j <= m; j++) {
-    dp[i][j] =
-      Math.max(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) +
-      maze[i - 1][j - 1];
+function fib(n) {
+  if (n === 1 || n === 2) {
+    fibCount += 1;
+    return 1;
+  } else {
+    return fib(n - 1) + fib(n - 2);
   }
 }
-console.log(dp[n][m]);
+
+function fibonacci(n) {
+  const dp = new Array(n + 1).fill(0);
+  dp[1] = dp[2] = 1;
+  for (let i = 3; i <= n; i++) {
+    dp[n] = dp[n - 1] + dp[n - 2];
+    fibonacciCount += 1;
+  }
+}
+fib(n);
+fibonacci(n);
+console.log(fibCount, fibonacciCount);
