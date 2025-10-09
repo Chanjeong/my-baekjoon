8a9fe849501cbf483a5328221a5148f1b1809a2f
chanjeong park <ckswjd9595@gmail.com>
2025-10-09 15:37:01 +0900

    DP - 17070 - 파이프 옮기기 1 @

--- index.js
+++ index.js
@@ -5,41 +5,46 @@ let input = require('fs')
   .split('\n');
 // let input = require("fs").readFileSync("/dev/stdin").toString().trim().split('\n');
 
-const [str1, str2] = input.slice(0).map(e => e.trim());
+const n = +input[0];
 
-const dp = Array.from({ length: str1.length + 1 }, () =>
-  new Array(str2.length + 1).fill(0)
+const house = input.slice(1).map(e => e.split(' ').map(Number));
+
+const dp = Array.from({ length: n + 1 }, () =>
+  Array.from({ length: n + 1 }, () => new Array(3).fill(0))
 );
 
-for (let i = 1; i <= str1.length; i++) {
-  for (let j = 1; j <= str2.length; j++) {
-    if (str1[i - 1] === str2[j - 1]) {
-      dp[i][j] = dp[i - 1][j - 1] + 1;
-    } else {
-      dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+dp[1][2][0] = 1;
+
+// 첫 행 초기화 (가로로만 이동 가능)
+for (let j = 3; j <= n; j++) {
+  if (house[0][j - 1] === 0) {
+    dp[1][j][0] = dp[1][j - 1][0];
+  }
+}
+
+//0 = 가로, 1 = 세로, 2 = 대각선
+for (let i = 2; i <= n; i++) {
+  for (let j = 2; j <= n; j++) {
+    // 현재 칸이 벽이면 스킵
+    if (house[i - 1][j - 1] === 1) continue;
+
+    // 가로(0): 왼쪽에서 가로 또는 대각선으로 올 수 있음
+    if (house[i - 1][j - 1] === 0) {
+      dp[i][j][0] = dp[i][j - 1][0] + dp[i][j - 1][2];
+      dp[i][j][1] = dp[i - 1][j][1] + dp[i - 1][j][2];
+    }
+
+    // 대각선(2): 3칸 모두 비어있어야 함
+    if (
+      house[i - 1][j - 1] === 0 &&
+      house[i - 2][j - 1] === 0 &&
+      house[i - 1][j - 2] === 0
+    ) {
+      dp[i][j][2] =
+        dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2];
     }
   }
 }
 
-let maxLength = dp[str1.length][str2.length];
-let result = '';
-let i = str1.length;
-let j = str2.length;
-
-while (i > 0 && j > 0) {
-  if (str1[i - 1] === str2[j - 1]) {
-    result = str1[i - 1] + result;
-    i--;
-    j--;
-  } else {
-    if (dp[i - 1][j] > dp[i][j - 1]) {
-      i--;
-    } else {
-      j--;
-    }
-  }
-}
-console.log(maxLength);
-if (maxLength) {
-  console.log(result);
-}
+const result = dp[n][n].reduce((acc, cum) => acc + cum, 0);
+console.log(result);
