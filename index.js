68bf89025625735d601f008b397d07afdaacc189
chanjeong park <ckswjd9595@gmail.com>
2025-10-17 14:42:03 +0900

    자료구조 - 1158 - 요세푸스 @

--- index.js
+++ index.js
@@ -5,30 +5,19 @@ let input = require('fs')
   .split('\n');
 // let input = require("fs").readFileSync("/dev/stdin").toString().trim().split('\n');
 
-const n = +input[0];
-const arr = input.slice(1).map(Number);
+const [N, K] = input[0].split(' ').map(Number);
 
-let stack = [];
-let current = 1;
-let result = [];
-
-for (let i = 0; i < n; i++) {
-  const target = arr[i];
-
-  // 목표 숫자까지 push
-  while (current <= target) {
-    stack.push(current++);
-    result.push('+');
-  }
-
-  // 스택의 top이 목표와 같으면 pop
-  if (stack[stack.length - 1] === target) {
-    stack.pop();
-    result.push('-');
-  } else {
-    console.log('NO');
-    process.exit(0);
-  }
+const people = [];
+for (let i = 1; i <= N; i++) {
+  people.push(i);
 }
 
-console.log(result.join('\n'));
+let result = [];
+let index = 0;
+while (people.length !== 1) {
+  index = (index + K - 1) % people.length;
+  result.push(people.splice(index, 1)[0]);
+}
+result.push(people.pop());
+
+console.log(result);
