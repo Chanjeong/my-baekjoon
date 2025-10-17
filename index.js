df6a58be5c386dd2a37dc4b5802d5b97afe1dcd2
chanjeong park <ckswjd9595@gmail.com>
2025-10-17 15:07:55 +0900

    자료구조 - 10866 - 데큐

--- index.js
+++ index.js
@@ -5,19 +5,36 @@ let input = require('fs')
   .split('\n');
 // let input = require("fs").readFileSync("/dev/stdin").toString().trim().split('\n');
 
-const [N, K] = input[0].split(' ').map(Number);
-
-const people = [];
-for (let i = 1; i <= N; i++) {
-  people.push(i);
-}
-
+const n = +input[0];
+let deque = [];
 let result = [];
-let index = 0;
-while (people.length !== 1) {
-  index = (index + K - 1) % people.length;
-  result.push(people.splice(index, 1)[0]);
+for (let i = 1; i <= n; i++) {
+  const [order, num] = input[i].trim().split(' ');
+  switch (order) {
+    case 'push_front':
+      deque.unshift(+num);
+      break; // 이게 핵심!
+    case 'push_back':
+      deque.push(+num);
+      break;
+    case 'pop_front':
+      result.push(deque.length ? deque.shift() : -1);
+      break;
+    case 'pop_back':
+      result.push(deque.length ? deque.pop() : -1);
+      break;
+    case 'size':
+      result.push(deque.length);
+      break;
+    case 'empty':
+      result.push(deque.length ? 0 : 1);
+      break;
+    case 'front':
+      result.push(deque.length ? deque[0] : -1);
+      break;
+    case 'back':
+      result.push(deque.length ? deque[deque.length - 1] : -1);
+      break;
+  }
 }
-result.push(people.pop());
-
-console.log(result);
+console.log(result.join('\n'));
