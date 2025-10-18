893b9ecb8a10e67df8ead33a56e175d361477797
chanjeong park <ckswjd9595@gmail.com>
2025-10-18 12:03:11 +0900

    자료구조 - 1927 - 최소힙(시간초과가 나지만 이 정답으로 유지) @

--- index.js
+++ index.js
@@ -6,30 +6,26 @@ let input = require('fs')
 // let input = require("fs").readFileSync("/dev/stdin").toString().trim().split('\n');
 
 const n = +input[0];
+const heap = [];
 
-for (let i = 1; i < input.length; i += 2) {
-  const [num, print] = input[i].split(' ').map(Number);
-  const priorities = input[i + 1].split(' ').map(Number);
+for (let i = 1; i <= n; i++) {
+  const num = +input[i];
 
-  const queue = [];
-  for (let j = 0; j < num; j++) {
-    queue.push({ index: j, priority: priorities[j] });
-  }
+  if (num === 0) {
+    console.log(heap.length ? heap.shift() : 0);
+  } else {
+    let left = 0;
+    let right = heap.length;
 
-  let count = 0;
+    while (left < right) {
+      let mid = Math.floor((left + right) / 2);
 
-  while (queue.length > 0) {
-    const current = queue.shift();
-
-    const hasHigherPriority = queue.some(e => e.priority > current.priority);
-    if (hasHigherPriority) {
-      queue.push(current);
-    } else {
-      count += 1;
-      if (current.index === print) {
-        console.log(count);
-        break;
+      if (heap[mid] < num) {
+        left = mid + 1;
+      } else {
+        right = mid;
       }
     }
+    heap.splice(left, 0, num);
   }
 }
